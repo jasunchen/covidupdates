de89d92854d6894e0b5a561a46d2608c4f534ddd
Angela Chen <achen0816@berkeley.edu>
2020-09-06 06:19:31 -0400

    Add cache of times and locations of past two weeks

--- CovidUpdates/app/src/main/java/com/example/covidupdates/LocationService.java
+++ CovidUpdates/app/src/main/java/com/example/covidupdates/LocationService.java
@@ -2,6 +2,7 @@ package com.example.covidupdates;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
@@ -14,10 +15,23 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LocationService extends Service {
 
     private FusedLocationProviderClient fusedLocationClient;
     private LocationCallback locationCallback;
+
+    private static final long INTERVAL = 5000;
+    private static final long MILLISECOND_IN_TWO_WEEKS = 1000*60*60*24*14;
+    private static final long NUM_TIMES_IN_PAST_TWO_WEEKS = MILLISECOND_IN_TWO_WEEKS / INTERVAL;
+
+    private LinkedHashMap<Long, Location> timesOfPastTwoWeeks = new LinkedHashMap<Long, Location>() {
+        protected boolean removeEldestEntry(Map.Entry<Long, Location> eldest) {
+            return size() > (int) NUM_TIMES_IN_PAST_TWO_WEEKS;
+        }
+    };
 
     @Nullable
     @Override
@@ -34,6 +48,7 @@ public class LocationService extends Service {
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 Log.d("logLocation", "Lat: "+locationResult.getLastLocation().getLatitude()+", Long: "+locationResult.getLastLocation().getLongitude());
+                timesOfPastTwoWeeks.put(locationResult.getLastLocation().getTime(), locationResult.getLastLocation());
             }
         };
     }
@@ -46,7 +61,7 @@ public class LocationService extends Service {
 
     private void requestLocation() {
         LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(5000);
+        locationRequest.setInterval(INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
     }
